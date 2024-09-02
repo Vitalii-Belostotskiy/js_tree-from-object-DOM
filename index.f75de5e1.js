a3d6060eca974ead6076ce2cfcb74a01da6a8e6c
Vitalii Bilostotskyi <belostotskiyvitalii84@gmail.com>
2024-09-02 16:49:39 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,37 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+function createTree(element, data) {
+    // WRITE YOUR CODE HERE
+    element.append(createElement(data));
+}
+const tree = document.querySelector("#tree");
+function createElement(obj) {
+    if (!Object.keys(obj).length) return;
+    const ul = document.createElement("ul");
+    for(const key in obj){
+        const li = document.createElement("li");
+        li.innerHTML = key;
+        const chaildUl = createElement(obj[key]);
+        if (chaildUl) li.append(chaildUl);
+        ul.append(li);
+    }
+    return ul;
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
